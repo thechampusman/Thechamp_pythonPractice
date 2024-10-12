ca1877ed9962597e8727b0c396c7994ed04afb3c
champ0011 <usmangour100@gmail.com>
2024-10-12 15:15:34 +0530

    12-10-24

---
+++ veryBasic/areaOfCuboid.py
@@ -0,0 +1,7 @@
+l = float(input("Enter the length of the cuboid: "))
+h = float(input("Enter the height of the cuboid: "))
+b = float(input("Enter the breath of the cuboid: "))
+
+area = 2* ((l*h)+(l*b)+(b*h))
+
+print("The area of the cuboid is", area)
---
+++ veryBasic/covertingKilometers_to_Miles.py
@@ -0,0 +1,3 @@
+km = float(input("Enter kilometers: "))
+miles = km * 0.621371
+print("Total miles" , miles)
